71b12b06953287b87768c747eb47a30922eafcb1
Wills Blake <richwblake@gmail.com>
2019-11-06 03:51:14 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -39,5 +39,5 @@ def sum_array( arr )
 end
 
 def add_s( arr )
-  arr.collect { |string| string.insert( -1, "s") if arr.find_index( string ) == 1 }
+  arr.collect { |string| string.insert( -1, "s") unless arr.find_index( string ) == 1 }
 end
