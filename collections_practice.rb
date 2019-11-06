99486fbf14a6d6f5db4854c759efcbc99e0349a1
Wills Blake <richwblake@gmail.com>
2019-11-06 02:32:22 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,21 @@
+def sort_array_asc( arr )
+  arr.sort
+end
 
+def sort_array_desc( arr )
+  arr.sort.reverse
+end
+
+def sort_array_char_count( arr )
+  arr.sort { |a, b| a.length <=> b.length }
+end
+
+def swap_elements( arr )
+  value = arr.delete_at( -2 )
+  arr.push( value )
+  arr
+end
+
+def reverse_array( arr )
+  arr.reverse
+end
